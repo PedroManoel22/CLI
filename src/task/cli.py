cc84413136898ff3ab1b031d25b104d8a1e01e55
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-07-24 11:35:12 -0300

    implementa subcomando search

--- src/task/cli.py
+++ src/task/cli.py
@@ -5,7 +5,7 @@ import rich_argparse
 
 from task.runners import DefaultRunner
 from task.settings import clear
-from task.validation import validate_str
+from task.validation import validate_positive_int, validate_str
 
 
 def build_parser() -> None:
@@ -92,6 +92,69 @@ def build_parser() -> None:
         help="Show all task",
         description=textwrap.dedent("Show all task"),
         formatter_class=rich_argparse.RawDescriptionRichHelpFormatter,
+    )
+
+    # PARSER SEARCH
+
+    search_parser = subparsers.add_parser(
+        "search",
+        aliases=["find"],
+        description=textwrap.dedent("""
+        Use this command to search and filter tasks based on specific criteria.
+
+        Quickly locate tasks by keyword, tag, priority, or completion status. 
+        Whether you are looking for urgent items or reviewing completed work, 
+        this command gives you full visibility over your task list."""),
+        epilog=textwrap.dedent("""
+            Examples:
+    
+            task search -t "Buy groceries"
+            task search -t "Study argparse" --tag python --tag cli --priority high
+            task search -t "Walk the dog" --done
+    
+            You can also combine options freely to match your workflow.
+            Tags help with filtering later. Priorities can be: low, medium, high."""),
+        formatter_class=rich_argparse.RawDescriptionRichHelpFormatter,
+    )
+    search_parser.set_defaults(command="search")
+
+    search_parser.add_argument(
+        "-t",
+        "--task",
+        type=validate_str,
+        help="Searches for your task description (regular expression allowed)",
+    )
+
+    search_parser.add_argument(
+        "--done",
+        help="Finds task with '--don' (completed) or '--no-done' (not completed)",
+        action=argparse.BooleanOptionalAction,
+    )
+
+    search_parser.add_argument(
+        "--tag",
+        help="Adds tags to your tasks for organization",
+        action="extend",
+        type=validate_str,
+        nargs="*",  # zero ou muitos
+        default=None,
+        dest="tags",
+    )
+
+    search_parser.add_argument(
+        "-p",
+        "--priority",
+        choices=["low", "medium", "high"],
+        default="medium",
+        help="Sets the priority for your task",
+    )
+
+    search_parser.add_argument(
+        "-l",
+        "--limit",
+        help="Limits the number of tasks per search",
+        default=10,
+        type=validate_positive_int,
     )
 
     return parser
